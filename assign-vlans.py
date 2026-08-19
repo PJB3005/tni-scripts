5bde2935cfef2fd04317b463e7fd2ddb2bcd8f59
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2026-08-19 22:42:16 +0200

    assign-vlans router

--- assign-vlans.py
+++ assign-vlans.py
@@ -12,6 +12,12 @@ def main() -> int:
     parser_switch.add_argument("ports")
     parser_switch.add_argument("--trunk")
 
+    parser_router = subparsers.add_parser("router")
+    parser_router.set_defaults(func=cmd_router)
+    parser_router.add_argument("addr")
+    parser_router.add_argument("ports")
+    parser_router.add_argument("trunk")
+
     args = parser.parse_args()
     return args.func(args) or 0
 
@@ -23,6 +29,16 @@ def cmd_switch(args: argparse.Namespace):
     if trunk := args.trunk:
         all_vlans = ' '.join(map(lambda p: f"#{p}", ports))
         print(f"vlan tag port{trunk} with {all_vlans} on {args.addr}; ", end="")
+
+def cmd_router(args: argparse.Namespace):
+    trunk = int(args.trunk)
+
+    ports = parse_ports(args.ports)
+    all_vlans = ' '.join(map(lambda p: f"#{p}", ports))
+    print(f"vlan tag port{trunk} with {all_vlans} on {args.addr}; ", end="")
+
+    for port in ports:
+        print(f"vlan tag port{trunk}.{port + 1} with #{port} on {args.addr}; ", end="")
 
 def parse_ports(arg: str) -> set[int]:
     arg = arg.strip()
